82f39ec92ae6cd1a856dd7d2da9ac42871b93ce3
ichiyasan <zipporahdean123@gmail.com>
2024-10-05 22:25:43 +0800

    Add files via upload

---
+++ data strat/cc104.js
@@ -0,0 +1,6 @@
+let courses = ["Information Technology" , "Agriculture" ,
+    "Hospitality Management", "Computer Science"]
+    
+    courses.forEach(Element {
+        console.log(element);
+    });
